cc68fc7655445a4d6eda43a39b391cdf86917fe2
MTxSouza <msouza.os@hotmail.com>
2022-07-04 09:58:13 -0300

    Fixing setup file.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="VGG16-Architecture",
-    version="0.1.1",
+    version="0.1.0",
     author="Matheus Oliveira de Souza",
     description="The VGG16 architecture, a neural net which classify images, made entirely with Tensorflow 2.x from scratch.",
     long_description=open("README.md", "r").read(),
-    install_requires=[
+    install_packages=[
         "tensorflow",
         "opencv-python",
         "matplotlib"
